f5c6d0596bd6f471d372036d7ae8be1b7f0a39ef
FLORIN RINJA <FRINJA@mapal-os.com>
2024-06-04 23:06:08 +0200

    feat(generation): added options for token generation;

--- index.js
+++ index.js
@@ -3,22 +3,22 @@ const exec   = require('child_process').exec;
 const jwa    = require('jwa');
 const cache  = require('kitten-cache');
 
-const ALGORITHM_BITS = '512';
-const ALGORITHM_NAME = 'ES'+ALGORITHM_BITS;
-const ALGORITHM_SIGN = 'secp521r1'; // 'prime256v1';
-const ecdsa          = jwa(ALGORITHM_NAME);
+const DEFAULT_ALGORITHM_BITS = '512';
+const DEFAULT_ALGORITHM_NAME = 'ES' + DEFAULT_ALGORITHM_BITS;
+const DEFAULT_ALGORITHM_SIGN = 'secp521r1'; // 'prime256v1';
+const ALLOWED_ALGORITHMS     = ['HS256', 'HS384', 'HS512', 'RS256', 'RS384', 'RS512', 'PS256', 'PS384', 'PS512', 'ES256', 'ES384', 'ES512'];
 
 const TOKEN_COOKIE_REGEXP = /access_token\s*=([^;]+?)(?:;|$)/;
 
-let params = {
+const params = {
   // client cache size used by getToken
-  clientCacheSize : 255,
+  clientCacheSize                        : 255,
   // how many time before expiration do we renew the token in millisecond
-  clientRenewTokenBeforeExp : 60 * 20 * 1000,
+  clientRenewTokenBeforeExp              : 60 * 20 * 1000,
   // default expiration of token in seconds
-  clientTokenExpiration : 60 * 60 * 12,
+  clientTokenExpiration                  : 60 * 60 * 12,
   // server cache size used by verifyHTTPHeaderFn
-  serverCacheSize : 255,
+  serverCacheSize                        : 255,
   // Invalidate bad token cache after XX milliseconds when the error is coming from getPublicKey
   serverGetPublicKeyErrorCacheExpiration : 120 * 1000
 };
@@ -42,9 +42,9 @@ let serverCache = new cache({size : params.serverCacheSize});
  *                              }
  */
 function set (options) {
-  for(let _attr in options) {
+  for (const _attr in options) {
     if (params[_attr] !== undefined) {
-      params[_attr] = options[_attr]
+      params[_attr] = options[_attr];
     }
   }
   // reset cache only if clientCacheSize or serverCacheSize changes
@@ -61,7 +61,7 @@ function set (options) {
  * @return {String}
  */
 function base64urlDecode (str) {
-  let _str = str.replace(/\-/g, '+').replace(/_/g, '/');
+  const _str = str.replace(/-/g, '+').replace(/_/g, '/');
   return Buffer
     .from(_str, 'base64')
     .toString('utf8');
@@ -90,8 +90,8 @@ function base64urlEncode (str) {
  * @param  {Function} callback(err, stderr, stdout)      
  */
 function generateECDHKeys (outputDir, outputKeyName, callback) {
-  let _filepath = path.join(outputDir, outputKeyName);
-  let _cmdline  = `openssl ecparam -name ${ALGORITHM_SIGN} -out ${_filepath}-temp.pem && openssl ecparam -in ${_filepath}-temp.pem -genkey -noout -out ${_filepath}.pem && openssl ec -in ${_filepath}.pem -pubout -out ${_filepath}.pub && rm ${_filepath}-temp.pem`;
+  const _filepath = path.join(outputDir, outputKeyName);
+  const _cmdline  = `openssl ecparam -name ${DEFAULT_ALGORITHM_SIGN} -out ${_filepath}-temp.pem && openssl ecparam -in ${_filepath}-temp.pem -genkey -noout -out ${_filepath}.pem && openssl ec -in ${_filepath}.pem -pubout -out ${_filepath}.pub && rm ${_filepath}-temp.pem`;
   exec(_cmdline, function (err, stdout, stderr) {
     if (err) {
       console.log('WARNING: cannot generate ECDH keys:' + err);
@@ -103,37 +103,47 @@ function generateECDHKeys (outputDir, outputKeyName, callback) {
 /**
  * Generate a token
  * 
- * @param  {Mixed}   clientId
- * @param  {Mixed}   serverId
- * @param  {Integer} expiresIn in seconds
- * @param  {String}  privKey
- * @param  {Mixed}   data
- * @return {String}  return the token
+ * @param {{
+ *  header : {
+ *    alg : String,
+ *    typ : String,
+ *    kid : String
+ *  },
+ *  payload : {
+ *    clientId  : Mixed,
+ *    serverId  : Mixed,
+ *    expiresIn : Integer, // in seconds
+ *  }
+ *  privKey   : String, 
+ * }} options 
+ *    
+ * @param   {Mixed}   data 
+ * @returns {String}  return the token
  */
-function generate (clientId, serverId, expiresIn, privKey, data) {
-  let _now = Math.floor(Date.now() / 1000);
-  
-  let _header = {
-    alg : ALGORITHM_NAME,
-    typ : 'JWT'
+function generate (options, data) {
+  const _now = Math.floor(Date.now() / 1000);
+  const _header = {
+    alg : options?.header?.alg || DEFAULT_ALGORITHM_NAME,
+    typ : options?.header?.typ || 'JWT',
+    ...(options?.header?.kid) && { kid : options.header.kid },
   };
   // generate a compact token must be compact
-  let _token = {
-    iss : clientId,
-    aud : serverId,
-    exp : (_now + expiresIn)
+  const _token = {
+    iss : options.payload.clientId,
+    aud : options.payload.serverId,
+    exp : (_now + options.payload.expiresIn)
   };
 
   if (data) {
     _token.data = data;
   }
-
-  let _tokenString = base64urlEncode(JSON.stringify(_header))+'.'+base64urlEncode(JSON.stringify(_token));
-  let _signature   = ecdsa.sign(_tokenString, privKey);
+  
+  const _tokenString = base64urlEncode(JSON.stringify(_header))+'.'+base64urlEncode(JSON.stringify(_token));
+  const _signature   = jwa(options?.header?.alg || DEFAULT_ALGORITHM_NAME).sign(_tokenString, options.privKey);
   // I should be able to use only nodejs, but there is something which does not follow RFCs
   // let _signature = crypto.createSign('SHA'+ALGORITHM_HASH).update(_tokenString).sign(privKey, 'base64');
   // let _signature64 = formatEcdsa.joseToDer(base64urlEncode(_signature), 'ES' + ALGORITHM_HASH);
-  let _tokenBase64WithSignature = _tokenString + '.' + _signature;
+  const _tokenBase64WithSignature = _tokenString + '.' + _signature;
 
   return _tokenBase64WithSignature;
 }
@@ -146,22 +156,22 @@ function generate (clientId, serverId, expiresIn, privKey, data) {
  * @param  {Integer}  now      Current timestamp in ms
  */
 function parseToken (jwt, callback, now = Date.now()) {
-  let _segments = jwt.split('.');
+  const _segments = jwt.split('.');
   if (_segments.length !== 3) {
     return callback(new Error('Invalid JSON Web Token: Not enough or too many segments'));
   }
-  let _headerBase64  = _segments[0];
-  let _payloadBase64 = _segments[1];
-  let _signature     = _segments[2];
-  let _tokenString   = _headerBase64 + '.' + _payloadBase64;
-  let _header        = null;
-  let _payload       = null;
+  const _headerBase64  = _segments[0];
+  const _payloadBase64 = _segments[1];
+  const _signature     = _segments[2];
+  const _tokenString   = _headerBase64 + '.' + _payloadBase64;
+  let _header          = null;
+  let _payload         = null;
 
   try {
-    let _headerString  = base64urlDecode(_headerBase64);
-    let _payloadString = base64urlDecode(_payloadBase64);
-    _header            = JSON.parse(_headerString);
-    _payload           = JSON.parse(_payloadString);
+    const _headerString  = base64urlDecode(_headerBase64);
+    const _payloadString = base64urlDecode(_payloadBase64);
+    _header              = JSON.parse(_headerString);
+    _payload             = JSON.parse(_payloadString);
   }
   catch (e) {
     return callback(new Error('Invalid JSON Web Token: ' + e.message));
@@ -175,42 +185,47 @@ function parseToken (jwt, callback, now = Date.now()) {
     return callback(new Error('Invalid Header JSON Web Token. It is not an object'));
   }
 
-  if (_header.alg !== ALGORITHM_NAME) {
-    return callback(new Error('Algorithm not accepted for JSON Web Token. Only ' + ALGORITHM_NAME + ' is accepted'));
+  if (!ALLOWED_ALGORITHMS.includes(_header.alg)) {
+    return callback(new Error(`Algorithm not accepted for JSON Web Token. Only ${ALLOWED_ALGORITHMS.join(', ')} are accepted`));
   }
 
   if (_payload.exp && now > parseInt(_payload.exp, 10) * 1000) {
-    return callback(new Error('JSON Web Token expired'), _payload);
+    console.log('parse',{_payload});
+    return callback(new Error('JSON Web Token expired'), _header, _payload);
   }
 
+  console.log('after_parse',{_payload});
   if (_payload.iss === '' || _payload.iss === undefined || _payload.iss === null) {
     return callback(new Error('JSON Web Token without issuer'));
   }
-  return callback(null, _payload, _tokenString, _signature);
+
+  return callback(null, _header, _payload, _tokenString, _signature);
 }
 
 /**
  * Verify the token
  * 
+ * @param  {Object}   header      parsed token by parseToken
  * @param  {Object}   payload     parsed token by parseToken
  * @param  {[type]}   tokenString token base64 string
  * @param  {[type]}   signature   signature only
  * @param  {[type]}   publicKey   public key
  * @param  {Function} callback(err, parsedPayload)
  */
-function verifyToken (payload, tokenString, signature, publicKey, callback) {
+function verifyToken (header, payload, tokenString, signature, publicKey, callback) {
   try {
-    let _isValidSignature = ecdsa.verify(tokenString, signature, publicKey);
+    console.log({header});
+    const _isValidSignature = jwa(header?.alg || DEFAULT_ALGORITHM_NAME).verify(tokenString, signature, publicKey);
     // let _verifier = crypto.createVerify('RSA-SHA'+ALGORITHM_HASH).update(tokenString);
     // let _isValidSignature = _verifier.verify(publicKey, signature, 'base64');
 
     if (_isValidSignature === false) {
       return callback(new Error('Invalid JSON Web Token signature'));
     }
-
   } catch (e) {
     return callback(new Error('Invalid JSON Web Token: ' + e.message));
   }
+
   return callback(null, payload);
 }
 
@@ -223,11 +238,12 @@ function verifyToken (payload, tokenString, signature, publicKey, callback) {
  * @param  {Integer}  now       Current timestamp in ms
  */
 function verify (jwt, publicKey, callback, now = Date.now()) {
-  parseToken(jwt, (err, payload, tokenString, signature) => {
+  parseToken(jwt, (err, header, payload, tokenString, signature) => {
     if (err) {
       return callback(err, payload);
     }
-    return verifyToken(payload, tokenString, signature, publicKey, callback);
+
+    return verifyToken(header, payload, tokenString, signature, publicKey, callback);
   }, now);
 }
 
@@ -240,14 +256,23 @@ function verify (jwt, publicKey, callback, now = Date.now()) {
  * @param  {Object} data     user data
  */
 function getToken (clientId, serverId, privKey, data) {
-  let _cacheKey = clientId + '_' + serverId;
+  const _cacheKey = clientId + '_' + serverId;
 
-  let _cachedToken = clientCache.get(_cacheKey);
-  let _now = Date.now();
+  const _cachedToken = clientCache.get(_cacheKey);
+  const _now = Date.now();
   if ( _cachedToken !== undefined && _now < (_cachedToken.expireAt - params.clientRenewTokenBeforeExp) && _cachedToken.privKey === privKey ) {
     return _cachedToken.token;
   }
-  let _newToken = generate(clientId, serverId, params.clientTokenExpiration, privKey, data);
+
+  const options = {
+    payload : {
+      clientId,
+      serverId, 
+      expiresIn : params.clientTokenExpiration
+    },
+    privKey
+  };
+  const _newToken = generate(options, data);
 
   clientCache.set(_cacheKey, {
     token    : _newToken,
@@ -269,15 +294,15 @@ function getToken (clientId, serverId, privKey, data) {
  * @param  {Function} callback(err) 
  * @param  {Number}   i             iterator of public keys
  */
-function verifyTokenForEachPublicKey (publicKeys, payload, tokenString, signature, callback, i = 0) {
-  verifyToken(payload, tokenString, signature, publicKeys[i], (err) => {
+function verifyTokenForEachPublicKey (publicKeys, header, payload, tokenString, signature, callback, i = 0) {
+  verifyToken(header, payload, tokenString, signature, publicKeys[i], (err) => {
     i++;
     if (!err || i >= publicKeys.length) {
       return callback(err);
     }
     // avoid looping without releasing NodeJS event loop
     process.nextTick(() => {
-      verifyTokenForEachPublicKey(publicKeys, payload, tokenString, signature, callback, i);
+      verifyTokenForEachPublicKey(publicKeys, header, payload, tokenString, signature, callback, i);
     });
   });
 }
@@ -293,7 +318,7 @@ function findToken (req, callback) {
     return callback(new Error('JSON Web Token - No HTTP header detected'));
   }
   // Accept tokens in authorization header and cookies
-  let _token = req.headers.authorization || req.headers.Authorization || parseCookie(req.headers.cookie);
+  const _token = req.headers.authorization || req.headers.Authorization || parseCookie(req.headers.cookie);
 
   if (typeof _token !== 'string' || _token.length === 0) {
     return callback(new Error('No JSON Web Token detected in Authorization header or Cookie. Format is "Authorization: jwt" or "Cookie: access_token=jwt"'));
@@ -302,6 +327,7 @@ function findToken (req, callback) {
   if (/^Bearer /i.test(_token) === true) {
     return callback(null, _token.slice(7));
   }
+
   return callback(null, _token);
 }
 
@@ -312,7 +338,7 @@ function findToken (req, callback) {
  * @return {String}        jwt if found, null otherwise
  */ 
 function parseCookie (cookie) {
-  var _token = TOKEN_COOKIE_REGEXP.exec(cookie);
+  const _token = TOKEN_COOKIE_REGEXP.exec(cookie);
   if (_token instanceof Array && _token.length > 1) {
     return _token[1].trim();
   }
@@ -337,7 +363,7 @@ function verifyHTTPHeaderFn (serverId, getPublicKeyFn) {
       const _now = Date.now();
 
       // is it in cache, fast return of errors or not
-      let _cachedToken = serverCache.get(_token);
+      const _cachedToken = serverCache.get(_token);
       if (_cachedToken && (_cachedToken.expireAt === 0 || _cachedToken.expireAt > _now)) {
         if (_cachedToken.payload !== undefined && _cachedToken.payload.exp !== undefined && _now > parseInt(_cachedToken.payload.exp, 10) * 1000) {
           _cachedToken.err = new Error('JSON Web Token expired');
@@ -350,25 +376,25 @@ function verifyHTTPHeaderFn (serverId, getPublicKeyFn) {
       }
 
       // otherwise, compute everything
-      parseToken(_token, (err, payload, tokenString, signature) => {
+      parseToken(_token, (err, header, payload, tokenString, signature) => {
         if (err) {
           serverCache.set(_token, { payload : payload, err : err, expireAt : 0 });
           return next(err);
         }
         if (payload && payload.aud !== serverId) {
-          let _err = new Error('Invalid JSON Web Token audience');
+          const _err = new Error('Invalid JSON Web Token audience');
           serverCache.set(_token, { payload : payload, err : _err, expireAt : 0 });
           return next(_err);
         }
         getPublicKeyFn(req, res, payload, function (errPublicKey, publicKey) {
-          var _publicKeys = (publicKey instanceof Array) ? publicKey : [publicKey];
+          const _publicKeys = (publicKey instanceof Array) ? publicKey : [publicKey];
           if (errPublicKey || !publicKey || _publicKeys.length === 0) {
             // If we cannot get the public key, store this error in cache with an expiration date
             const _err = new Error('Empty public key or no public key available');
             serverCache.set(_token, { payload : payload, err : _err, expireAt : (_now + params.serverGetPublicKeyErrorCacheExpiration) });
             return next(_err);
           }
-          verifyTokenForEachPublicKey(_publicKeys, payload, tokenString, signature, (err) => {
+          verifyTokenForEachPublicKey(_publicKeys, header, payload, tokenString, signature, (err) => {
             // what ever happens, put result in cache
             serverCache.set(_token, { payload : payload, err : err, expireAt : 0 });
             if (err) {
